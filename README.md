153a5b4d7e9326ba74c3503fcdf34985d3946c21
Erick Agrazal <erick.agrazal@gmail.com>
2018-11-18 03:27:09 -0500

    New vagrant file

--- README.md
+++ README.md
@@ -9,12 +9,18 @@ Proyecto base para utilizar `JSP` con `TOMCAT` como app server y `HTTPD` como pr
 2. Instalar `docker-compose`
 3. Instalar `apache maven`
 
+### Instalación (Vagrant)
+1. Instalar `vagrant`.
+2. `vagrant up --provision  # Solo la primera vez`
+3. `cd /vagrant  # Es la carpeta definida como "shared" en el vagrantfile.`
+4. Construir la infraestructura.
+5. Compilar y ejecutar el proyecto. Repetir cada vez que se haga un cambio en un archivo JSP, HTML, CSS o JS.
+6. Digirse al navegador a la ruta: `http://localhost:8000/buscaminas/`. El puerto `8000` del host es redireccionado al puerto `80` del httpd que se encuentra dentro de la máquina virtual y ese a su vez, redirecciona ese puerto al `8009` de tomcat (Revisar `vagrantfile`, `Dockerfile`,  `docker-compose.yml`).
+ 
 ### Construcción de la infraestructura
 1. `docker-compose build  # Desde dentro de la carpeta del proyecto`
 
-### Compilación del proyecto
+### Compilación y ejecución del proyecto
 Este es un proceso que se debe repetir cada vez que se realice un cambio en el proyecto. 
 1. `mvn clean package  # Desde dentro de la carpeta del proyecto`
-
-### Ejecución
-1. `docker-compose up  # Desde dentro de la carpeta del proyecto`
+2. `docker-compose up  # Desde dentro de la carpeta del proyecto`
---
+++ initialize_server.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -x 
+
+function fix_previous_installs(){
+    echo 'Fixing previous installations...'
+    sudo dpkg --configure -a
+    sudo rm -rf /etc/apt/sources.list.d/webupd8team-java.list
+    sudo apt-get update
+    echo 'Finished fixing.'
+}
+
+function install_docker(){
+    echo 'Installing docker ...'
+    sudo add-apt-repository ppa:webupd8team/java -y
+    sudo apt-get -y update
+    sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-key  fingerprint 0EBFCD88
+    sudo add-apt-repository  -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get -y update
+    sudo apt-get -y install docker-ce
+    echo 'Finished docker install.'
+}
+
+function install_docker_compose(){
+    echo 'Installing docker-compose...'
+    sudo curl -L --fail https://github.com/docker/compose/releases/download/1.23.1/run.sh -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    echo 'Finished docker-compose install.'
+}
+
+function install_java(){
+    echo 'Installing Java...'
+    echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+    echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+    sudo apt-get install oracle-java8-installer -y
+    echo 'Finished Java install.'
+}
+
+function install_maven(){
+    echo 'Installing Maven...'
+    declare -r apacheMavenDownloadUrl="http://apache.cbox.biz/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
+    declare -r directoryName="mvn/"
+
+    if [[ $apacheMavenDownloadUrl =~ ([^/]+)\-bin\.tar\.gz$ ]]
+    then
+        declare -r resultFileName=$BASH_REMATCH
+        declare -r resultDirectoryName=${BASH_REMATCH[1]}
+    else
+        printf "\nIncorrcet format of Apache Maven download url!\n"
+        exit 1
+    fi
+
+    mkdir $directoryName
+    cd $directoryName
+
+    wget $apacheMavenDownloadUrl
+    tar -zxf $resultFileName
+    declare -r workingDir=$(pwd)
+
+    sudo ln -s "$workingDir/$resultDirectoryName/bin/mvn" /usr/local/bin/mvn
+    rm -v $resultFileName
+
+    echo 'Finished maven install.'
+}
+
+echo 'Initializing server...'
+fix_previous_installs
+install_docker
+install_docker_compose
+install_java
+install_maven
+echo 'Installation complete.'
